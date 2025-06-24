4e3a54aee84a7af54b651aba9cf1365c5463d530
kjinnuko <67011141@kmitl.ac.th>
2025-06-24 18:32:53 +0700

    good

--- cell04/ex00/upcase_it.py
+++ cell04/ex00/upcase_it.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
 
-def main():
-    w = input("Give me a word: ")
- 
-    print(w.upper())
-
-if __name__ == "__main__":
-    main()
+word = input("Give me a word: ")
+print(word.upper())
