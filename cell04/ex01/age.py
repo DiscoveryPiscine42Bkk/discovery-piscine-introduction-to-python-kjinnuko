13c93f89ee8d8bb49a7fae565e790d3d881671ce
kjinnuko <67011141@kmitl.ac.th>
2025-06-24 17:21:50 +0700

    good

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 
-def main():
-
-    a = int(input("Please tell me your age: "))
-    
-    print(f"You are currently {a} years old.")
-    print(f"In 10 years, you'll be {a + 10} years old.")
-    print(f"In 20 years, you'll be {a + 20} years old.")
-    print(f"In 30 years, you'll be {a + 30} years old.")
-
-if __name__ == "__main__":
-    main()
+#!/usr/bin/env python3
+age = int(input("Please tell me your age: "))
+print(f"You are currently {age} years old")
+for i in range(10 , 40, 10):
+    print(f"In {i} years, you'll be {i + age} years old.")
