94472b0644163378af451dde6ce343d066d8e430
kjinnuko <67011141@kmitl.ac.th>
2025-06-24 13:41:53 +0700

    good

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 try:
-    num = int(input())  
-    if num == 0:
-        print("This number is equal to zero.")
+    num = int(input())
+    if num < 0:
+        print("This number is negative.")
+    elif num > 0:
+        print("This number is positive.")
     else:
-        print("This number is different from zero.")
+        print("This number is both positive and negative.")
 except ValueError:
-    print("Not a number, please enter a number") 
+    print("Not a number, please enter a number.")
