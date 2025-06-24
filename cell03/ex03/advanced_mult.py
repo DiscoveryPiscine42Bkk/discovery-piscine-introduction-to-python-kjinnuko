8e8a10229508d10119532937529eb09c46bb0dbd
kjinnuko <67011141@kmitl.ac.th>
2025-06-24 16:40:42 +0700

    Good

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
+import sys
 
-def main():
-    n = 0  
-    while n <= 10:
-        print(f"Table de {n}:", " ".join(str(n * i) for i in range(11)))
-        n += 1 
-        
-if __name__ == "__main__":
-    main()
+if len(sys.argv) > 1:
+    print("none")
+    sys.exit()
+
+i = 0
+while i <= 10:
+    print(f"Table de {i}:", end=" ")
+
+    j = 0
+    while j <= 10:
+        print(i * j, end=" " if j < 10 else "")
+        j += 1
+
+    print()  
+    i += 1
