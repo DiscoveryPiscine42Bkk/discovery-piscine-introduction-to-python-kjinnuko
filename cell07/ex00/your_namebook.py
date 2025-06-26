0a5629cc26400f96ffb93a18c79ff2f5e6f83cf9
kjinnuko <67011141@kmitl.ac.th>
2025-06-26 05:19:05 +0000

    good

--- cell07/ex00/your_namebook.py
+++ cell07/ex00/your_namebook.py
@@ -7,12 +7,10 @@ def array_of_names(persons):
         full_names.append(f"{first_name.capitalize()} {last_name.capitalize()}")
     return full_names
 
-
 persons = {
     "jean": "valjean",
     "grace": "hopper",
     "xavier": "niel",
     "fifi": "brindacier"
 }
-
 print(array_of_names(persons))
