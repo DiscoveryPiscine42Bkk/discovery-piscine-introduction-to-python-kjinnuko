e29bfbd0f428044581e2bfa9ad47a5cc50ff02e6
kjinnuko <67011141@kmitl.ac.th>
2025-06-26 07:26:17 +0000

    good

--- cell07/ex03/persons_of_interest.py
+++ cell07/ex03/persons_of_interest.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
 
-def B(p):
-    
-    sorted_people = sorted(p.values(), key=lambda x: x["date_of_birth"])
+def famous_births(figures):
+    sorted_figures = sorted(
+        figures.values(), 
+        key=lambda person: int(person["date_of_birth"])
+    )
+    for person in sorted_figures:
+        print(f'{person["name"]} is a great scientist born in {person["date_of_birth"]}.')
 
-    for person in sorted_people:
-        print(f"{person['name']} is a great scientist born in {person['date_of_birth']}.")
+women_scientists = {
+    "ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
+    "cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
+    "lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
+    "grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
+}
 
-def main():
-    women_scientists = {
-        "ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
-        "cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
-        "lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
-        "grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
-    }
-    
-  
-    B(women_scientists)
-
-if __name__ == "__main__":
-    main()
+famous_births(women_scientists)
