99f6e02539bdac7481bab09cbd4a747210e59d96
kjinnuko <67011141@kmitl.ac.th>
2025-06-26 07:21:08 +0000

    good

--- cell07/ex02/help_your_professor.py
+++ cell07/ex02/help_your_professor.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 
-def average(class_scores):
-    
-    if not class_scores:
-        return 0  
-    return sum(class_scores.values()) / len(class_scores)
+def average(scores_dict):
+    total = sum(scores_dict.values())
+    count = len(scores_dict)
+    return total / count
 
+# ตัวอย่างการใช้งาน
 class_3B = {
     "marine": 18,
     "jean": 15,
     "coline": 8,
     "luc": 9
 }
-
 class_3C = {
     "quentin": 17,
     "julie": 15,
     "marc": 8,
     "stephanie": 13
 }
-
-print(f"Average for class 3B: {average(class_3B):.2f}.")
-print(f"Average for class 3C: {average(class_3C):.2f}.")
+print(f"Average for class 3B: {average(class_3B)}.")
+print(f"Average for class 3C: {average(class_3C)}.")
